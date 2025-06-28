db807c302c8ad4e0babf59801feb2bc99b5e661d
Basil Paul <basilmalikudy3@gmail.com>
2025-06-28 17:07:43 +0000

    Pre-Recovery checkpoint

--- replit.nix
+++ replit.nix
@@ -1,6 +1,12 @@
 { pkgs }: {
   deps = [
     pkgs.python311
-    pkgs.python311Packages.pip
+    pkgs.python311Packages.fastapi
+    pkgs.python311Packages.uvicorn
+    pkgs.python311Packages.requests
+    pkgs.python311Packages.jinja2
+    pkgs.python311Packages.python-multipart
+    pkgs.python311Packages.pydantic
+    pkgs.python311Packages.typing-extensions
   ];
 }
